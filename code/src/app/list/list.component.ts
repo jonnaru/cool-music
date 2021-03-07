3d08d50519ddbe288ebc19b0b5cfcbcb0acf80fe
jonna ru <jonna.ru@icloud.com>
2021-03-07 17:10:52 +0100

    fetching artists in list component

---
+++ code/src/app/list/list.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpService } from '../http.service'
+
+@Component({
+  selector: 'app-list',
+  templateUrl: './list.component.html',
+  styleUrls: ['./list.component.scss']
+})
+export class ListComponent implements OnInit {
+
+  artists: any; // Object?
+
+  constructor(private _http: HttpService) { }
+
+  ngOnInit(): void {
+    this._http.getMusic().subscribe(data => {
+      this.artists = data;
+      console.log(this.artists)
+    });
+  }
+
+}
