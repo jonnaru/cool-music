18f2c28d32a8e6a28724e7fe61d1eb8e67fe049c
jonna ru <jonna.ru@icloud.com>
2021-03-08 19:28:37 +0100

    basic artist cards

--- code/src/app/app.module.ts
+++ code/src/app/app.module.ts
@@ -4,12 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ListComponent } from './list/list.component';
-import { HttpClientModule } from '@angular/common/http'
+import { HttpClientModule } from '@angular/common/http';
+import { HeaderComponent } from './header/header.component';
+import { FilterbarComponent } from './filterbar/filterbar.component'
 
 @NgModule({
   declarations: [
     AppComponent,
-    ListComponent
+    ListComponent,
+    HeaderComponent,
+    FilterbarComponent
   ],
   imports: [
     BrowserModule,
