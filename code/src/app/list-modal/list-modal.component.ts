206094421d46d96ddb3b02440b35cc4305037392
jonna ru <jonna.ru@icloud.com>
2021-03-09 20:22:34 +0100

    video modal works

--- code/src/app/list-modal/list-modal.component.ts
+++ code/src/app/list-modal/list-modal.component.ts
@@ -1,21 +1,35 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
+
+const getId = (url: string) => {
+  const regExp = /^.*(youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=|&v=)([^#&?]*).*/;
+  const match = url.match(regExp);
+
+  return match && match[2].length === 11 ? match[2] : null;
+};
 
 @Component({
   selector: 'app-list-modal',
   templateUrl: './list-modal.component.html',
-  styleUrls: ['./list-modal.component.scss']
+  styleUrls: ['./list-modal.component.scss'],
 })
 export class ListModalComponent implements OnInit {
-
   @Input() videoLink: any;
-  
+  @Input() artistName: any;
+
   @Output() closeModalEvent = new EventEmitter();
 
+  url: string = '';
 
+  safeURL: any;
 
-  constructor() { }
+  constructor(private _sanitizer: DomSanitizer) {}
 
   ngOnInit(): void {
+    this.url = `https://www.youtube.com/embed/${getId(
+      this.videoLink
+    )}?controls=0&autoplay=1`;
+    this.safeURL = this._sanitizer.bypassSecurityTrustResourceUrl(this.url);
   }
 
   clickCloseModal() {
--- code/src/app/list/list.component.ts
+++ code/src/app/list/list.component.ts
@@ -1,36 +1,36 @@
 import { Component, OnInit } from '@angular/core';
-import { HttpService } from '../http.service'
+import { HttpService } from '../http.service';
 
 @Component({
   selector: 'app-list',
   templateUrl: './list.component.html',
-  styleUrls: ['./list.component.scss']
+  styleUrls: ['./list.component.scss'],
 })
 export class ListComponent implements OnInit {
-
   artists: any;
 
   showModal: boolean = false;
 
-  videoLink: String = "";
+  videoLink: string = '';
 
-  constructor(private _http: HttpService) { }
+  artistName: string = '';
+
+  constructor(private _http: HttpService) {}
 
   ngOnInit(): void {
-    this._http.getMusic().subscribe(data => {
+    this._http.getMusic().subscribe((data) => {
       this.artists = data;
-      console.log(this.artists)
+      console.log(this.artists);
     });
   }
 
-  handleClickVideo(videoLink: String) {
+  handleClickVideo(videoLink: string, artistName: string) {
     this.showModal = true;
-    this.videoLink = videoLink
+    this.videoLink = videoLink;
+    this.artistName = artistName;
   }
 
   closeModal() {
     this.showModal = false;
   }
-
-
 }
