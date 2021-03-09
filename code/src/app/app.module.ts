19ebc7c82453958a3171efbe64e341a1652fa0d5
jonna ru <jonna.ru@icloud.com>
2021-03-09 18:41:48 +0100

    card components and styling

--- code/src/app/app.module.ts
+++ code/src/app/app.module.ts
@@ -7,7 +7,11 @@ import { ListComponent } from './list/list.component';
 import { HttpClientModule } from '@angular/common/http';
 import { HeaderComponent } from './header/header.component';
 import { FilterbarComponent } from './filterbar/filterbar.component';
-import { ListModalComponent } from './list-modal/list-modal.component'
+import { ListModalComponent } from './list-modal/list-modal.component';
+import { PlayComponent } from './lib/play/play.component';
+import { IconButtonComponent } from './lib/icon-button/icon-button.component';
+import { ExitComponent } from './lib/exit/exit.component';
+// import { ModalComponent } from './modal/modal.component'
 
 @NgModule({
   declarations: [
@@ -15,7 +19,11 @@ import { ListModalComponent } from './list-modal/list-modal.component'
     ListComponent,
     HeaderComponent,
     FilterbarComponent,
-    ListModalComponent
+    ListModalComponent,
+    PlayComponent,
+    IconButtonComponent,
+    ExitComponent,
+    // ModalComponent
   ],
   imports: [
     BrowserModule,
@@ -26,3 +34,5 @@ import { ListModalComponent } from './list-modal/list-modal.component'
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+
